43b6fe8c6ab0798a3f08cc93b7eed3dd1fb7a07c
BT_1N3_34 <cstanilovsky9605@bths.edu>
2018-10-04 11:26:41 -0400

    Update with Person and Classroom classes

---
+++ src/com/company/Person.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public abstract class Person
+{
+    private String firstName, familyName;
+    public Person(String firstName, String familyName)
+    {
+        this.firstName = firstName;
+        this.familyName = familyName;
+    }
+    public abstract String toString();
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    public String getFamilyName()
+    {
+        return familyName;
+    }
+    public boolean equals(Person p)
+    {
+        if(this.firstName.equals(p.getFirstName()) && this.familyName.equals(p.getFamilyName()))
+        {
+            return true;
+        }else return false;
+    }
+}
