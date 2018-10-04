72613481678b32fd0b710dae455a1aa66d28aef1
Kyle <38015393+ProDevKyle@users.noreply.github.com>
2018-10-04 11:15:50 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,34 @@
-package com.company;
-
-public abstract class Person {
-    private String first;
-    private String last;
-    private int type;
-
-    public Person(String first, String last, String type){
-        this.first = first;
-        this.last = last;
-        this.type = type;
+public abstract class Person
+{
+    private String firstName, familyName;
+    public Person(String firstName, String familyName)
+    {
+        this.firstName = firstName;
+        this.familyName = familyName;
     }
-    if(type == 0){
-        public abstract String getFirstName();
-        public abstract String getFamilyName();
-        public abstract boolean equals(Person p);
-    }else if(type == 1){
-        public abstract int getGPA();
-        public abstract String toString();
-    }else if(type == 2){
-        public abstract String getSubject();
-        public abstract String toString();
-    }else{
-        public abstract String getSubject();
-        public abstract int classAverage();
-        public abstract String printClass();
+    public abstract String toString();
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+    public String getFamilyName()
+    {
+        return familyName;
+    }
+    public void setFamilyName(String familyName)
+    {
+        this.familyName = familyName;
+    }
+    public boolean equals(Person person)
+    {
+        if(this.firstName.equals(person.getFirstName()) && this.familyName.equals(person.getFamilyName()))
+        {
+            return true;
+        }
+        else return false;
     }
 }
